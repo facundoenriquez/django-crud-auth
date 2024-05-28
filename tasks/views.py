5489e95e44d473bc5a590c0af5266dc9d39d0292
Facundo Enriquez <fenriquez@inym.org.ar>
2024-05-28 13:55:11 -0300

    second commit

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
-from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from .forms import TaskForm
+from .models import Task
 
 # Create your views here.
 
@@ -37,8 +38,57 @@ def signup(request):
             'error': 'Password do not match'
         })
 
+
 def tasks(request):
-    if request.method == 'POST':
-        return HttpResponse('Task created successfully')
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'tasks.html', {
+        'tasks': tasks
+    })
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
     else:
-        return render(request, 'tasks.html')
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Invalid username or password'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
+
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+            'form': TaskForm
+        })
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {
+                'form': TaskForm,
+                'error': 'Error creating task'
+            })
+
+
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    form = TaskForm(instance=task)
+    return render(request, 'task_detail.html', {'task': task, 'form': form})
